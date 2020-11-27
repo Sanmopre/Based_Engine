9e0aed133b24d3dfdb4d2d0ff61e3300d53dca59
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-11-27 15:28:22 +0100

    starting to add new features to transfrom and using MathGeoLib

--- Based_Engine/Based_Engine/Transform.cpp
+++ Based_Engine/Based_Engine/Transform.cpp
@@ -5,6 +5,16 @@ Transform::Transform()
 	position = { 0,0,0 };
 	rotation = { 0,0,0 };
 	scale = { 1,1,1 };
+
+	local_position = { 0,0,0 };
+	local_rotation = { 0,0,0 };
+	local_scale = { 0,0,0 };
+
+	euler_rotation = local_rotation.ToEulerXYX();
+	euler_rotation.x = RadToDeg(euler_rotation.x);
+	euler_rotation.y = RadToDeg(euler_rotation.y);
+	euler_rotation.z = RadToDeg(euler_rotation.z);
+	local_transformation = float4x4::FromTRS(local_position, local_rotation, local_scale);
 }
 
 Transform::~Transform()
--- Based_Engine/Based_Engine/Transform.h
+++ Based_Engine/Based_Engine/Transform.h
@@ -1,5 +1,6 @@
 #include "Globals.h"
 #include "Math/float3.h"
+#include "MathGeoLib.h"
 
 
 struct Transform
@@ -9,6 +10,23 @@ struct Transform
 
 	float3 position, rotation, scale;
 
+	float3 forward = { 0,0,0 };
+	float3 up = { 0,0,0 };
+	float3 right = { 0,0,0 };
+
+	float4x4 global_transformation;
+	float4x4 local_transformation;
+
+	// position
+	float3 local_position = { 0,0,0 };
+	// sacale
+	float3 local_scale = { 0,0,0 };
+	// rotation
+	Quat local_rotation = { 0,0,0,0 };
+
+	float3 euler_rotation = { 0,0,0 };
+
+	//Operators
 	bool operator==(Transform t)
 	{
 		if (position.x == t.position.x && position.y == t.position.y && position.z == t.position.z &&
