2c950d98c34d3016524530e1c9a69a11296b4a7c
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-12-22 15:07:26 +0100

    Collider component structure

---
+++ Based_Engine/Based_Engine/ColliderComponent.h
@@ -0,0 +1,31 @@
+#ifndef __COLLIDERCOMPONENT_H__
+#define __COLLIDERCOMPONENT_H__
+
+#include "Component.h"
+
+enum class colider_type {
+BOX,
+SPHERE,
+CAPSULE,
+MESH,
+NONE
+};
+
+
+class ColliderComponent : public Component
+{
+public:
+
+	ColliderComponent(char* name,colider_type type, GameObject* parent, Application* app, bool active = true);
+	virtual ~ColliderComponent();
+
+	bool Update(float dt);
+
+	void DisplayComponentMenu();
+
+private:
+
+	colider_type type;
+};
+
+#endif //__COLLIDERCOMPONENT_H__
