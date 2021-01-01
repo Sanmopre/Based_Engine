9a42b84e77882256dc20b8429ce7a67554915fca
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2021-01-01 20:03:18 +0100

    collider creation funciotn (basic structure)

--- Based_Engine/Based_Engine/ColliderComponent.h
+++ Based_Engine/Based_Engine/ColliderComponent.h
@@ -2,6 +2,7 @@
 #define __COLLIDERCOMPONENT_H__
 
 #include "Component.h"
+#include "MathGeoLib.h"
 
 enum class colider_type {
 BOX,
@@ -23,6 +24,9 @@ namespace physx
 	typedef uint16_t PxU16;
 }
 
+class Mesh;
+
+
 class ColliderComponent : public Component
 {
 public:
@@ -34,6 +38,7 @@ public:
 	void Enable();
 	void Disable();
 
+	void CreateCollider(colider_type type, bool createAgain = false);
 	void UpdateCollider();
 
 	void UpdateLocalMatrix();
@@ -42,10 +47,40 @@ public:
 
 	void DisplayComponentMenu();
 
-private:
+public:
+	colider_type type = colider_type::NONE;
+	Mesh* mesh = nullptr;
+	Mesh* current_mesh = nullptr;
+	Mesh* dragged_mesh = nullptr;
+	bool editCollider = false;
+	bool updateValues = false;
+	float3 centerPosition = float3::zero;
+	float3 originalSize = float3::one;
+	physx::PxRigidStatic* rigidStatic = nullptr;
+	float3 offset = float3::zero;
+	int colliderType = 0;
+	bool hasBeenDeactivated = false;
+	bool isTrigger = false;
+	bool isConvex = false;
+	Quat dragged_rot = Quat::identity;
+	float3 dragged_scale = float3::one;
+	int dragged_UID = 0;
 
-	bool isTrigger;
-	colider_type type;
+
+private:
+	physx::PxShape* shape = nullptr;
+	physx::PxConvexMesh* convex_mesh = nullptr;
+	physx::PxTriangleMesh* triangle_mesh = nullptr;
+	float3 colliderSize = float3(1, 1, 1);
+	float4x4 localMatrix = float4x4::identity;
+	float4x4 globalMatrix = float4x4::identity;
+	float radius = 1.0f, height = 1.0f;
+	int lastIndex = -1;
+	float3 tmpScale = float3::one;
+	bool firstCreation = false;
+	bool toPlay = false;
+	bool draw = false;
+	bool localMesh = false;
 };
 
 #endif //__COLLIDERCOMPONENT_H__
