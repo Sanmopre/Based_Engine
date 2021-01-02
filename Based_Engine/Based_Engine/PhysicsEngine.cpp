57a9f1a0e9d8094017a7ede1ea6dffc6724436a8
Paideieitor <45202190+Paideieitor@users.noreply.github.com>
2021-01-02 14:26:24 +0100

    PhysX initalization is now more stable
    -the project now compiles in debug mode yay
    -erased redundant include files
    -simplified start function
    -made the module safer

--- Based_Engine/Based_Engine/PhysicsEngine.cpp
+++ Based_Engine/Based_Engine/PhysicsEngine.cpp
@@ -4,18 +4,7 @@
 #include "Application.h"
 #include "GameObject.h"
 
-#include "extensions/PxDefaultAllocator.h"
-#include "extensions/PxDefaultErrorCallback.h"
-#include "pvd/PxPvd.h"
-#include "pvd/PxPvdSceneClient.h"
-#include "pvd/PxPvdTransport.h"
 #include "PxPhysicsAPI.h"
-#include "characterkinematic/PxControllerManager.h"
-#include "foundation/PxAllocatorCallback.h"
-#include "PxQueryReport.h"
-#include "PxQueryFiltering.h"
-#include "extensions/PxRaycastCCD.h"
-#include "cooking/PxCooking.h"
 
 #define BOUNCE_THRESHOLD 0.2f
 #define THREADS 4
@@ -56,77 +45,39 @@ bool PhysicsEngine::Start()
 	foundation = PxCreateFoundation(PX_FOUNDATION_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
 	if (!foundation)
 	{
-		LOG("Error creating fundation");
+		LOG("PxCreateFoundation failed!");
+		return false;
+	}
+
+	bool recordMemoryAllocations = true;
+
+	physics = PxCreateBasePhysics(PX_PHYSICS_VERSION, *foundation, physx::PxTolerancesScale(), recordMemoryAllocations);
+	if (!physics)
+	{
+		LOG("PxCreatePhysics failed!");
 		return false;
 	}
 
 	cooking = PxCreateCooking(PX_PHYSICS_VERSION, *foundation, physx::PxCookingParams(physx::PxTolerancesScale()));
 	if (!cooking)
 	{
-		LOG("Error creating cooking");
+		LOG("PxCreateCooking failed!");
 		return false;
 	}
-
-	physx::PxCookingParams params = cooking->getParams();
-	params.convexMeshCookingType = physx::PxConvexMeshCookingType::eQUICKHULL;
-	params.gaussMapLimit = 32;
-	cooking->setParams(params);
-
-#ifdef _DEBUG
-	physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("localhost", 5425, 10000);
-	if (transport == NULL)
-	{
-		LOG("Error creating transport socket")
-		return false;
-	}
-
-	physx::PxPvdInstrumentationFlags pvdFlags = physx::PxPvdInstrumentationFlag::eALL;
-	pvd = physx::PxCreatePvd(*foundation);
-	pvd->connect(*transport, pvdFlags);
-
-	physics = PxCreateBasePhysics(PX_PHYSICS_VERSION, *foundation, physx::PxTolerancesScale(), true, pvd);
-#else
-	physics = PxCreateBasePhysics(PX_PHYSICS_VERSION, *foundation, physx::PxTolerancesScale(), true);
-#endif
-	
-	if (!physics) 
-	{
-		LOG("Error creating the base physics");
-		return false;
-	}
-
-	PxRegisterParticles(*physics);
-
 	physx::PxSceneDesc sceneDesc(physics->getTolerancesScale());
 	sceneDesc.gravity = physx::PxVec3(0.0f, -GRAVITY, 0.0f);
 	sceneDesc.bounceThresholdVelocity = GRAVITY * BOUNCE_THRESHOLD;
 	sceneDesc.cpuDispatcher = physx::PxDefaultCpuDispatcherCreate(THREADS);
 	sceneDesc.flags |= physx::PxSceneFlag::eENABLE_KINEMATIC_PAIRS | physx::PxSceneFlag::eENABLE_KINEMATIC_STATIC_PAIRS;
-
+	sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
 	scene = physics->createScene(sceneDesc);
-	if (!scene)
-	{
-		LOG("Error creating scene");
-		return false;
-	}
-
-#ifdef _DEBUG
-	scene->setVisualizationParameter(physx::PxVisualizationParameter::eSCALE, 1.0);
-	scene->setVisualizationParameter(physx::PxVisualizationParameter::eCOLLISION_SHAPES, 1.0f);
-	scene->setVisualizationParameter(physx::PxVisualizationParameter::eACTOR_AXES, 1.0f);
-
-	//Setup Configuration-----------------------------------------------------------------------
-	pvdClient = scene->getScenePvdClient();
-	if (pvdClient) 
-	{
-		pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-		pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-		pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
-	}
-	//-------------------------------------
-#endif
 
 	controllerManager = PxCreateControllerManager(*scene);
+	if (!controllerManager)
+	{
+		LOG("PxCreateControllerManager failed!");
+		return false;
+	}
 
 	return true;
 }
@@ -138,8 +89,11 @@ update_status PhysicsEngine::PreUpdate()
 
 update_status PhysicsEngine::Update(float dt)
 {
-	scene->simulate(dt);
-	scene->fetchResults(true);
+	if (scene)
+	{
+		scene->simulate(dt);
+		scene->fetchResults(true);
+	}
 
 	return UPDATE_CONTINUE;
 }
@@ -161,10 +115,8 @@ bool PhysicsEngine::CleanUp()
 		scene->release();
 	if (physics)
 		physics->release();
-#ifdef _DEBUG
 	if (pvd)
 		pvd->release();
-#endif
 	if (foundation)
 		foundation->release();
 	if (raycastManager)
